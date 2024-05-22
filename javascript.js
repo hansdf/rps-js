c05a84237adc670b3af3cbe69b7c7fc8af816d17
hansdf <dornsback@gmail.com>
2024-05-22 15:47:14 -0300

    final version for now

--- javascript.js
+++ javascript.js
@@ -1,92 +1,87 @@
-let userGuess
-let userScore = 0
-let cpuScore = 0
-let rounds = 0
+let userGuess;
+let rounds = 0;
+let resultWord;
+let cpuChoiceWord;
+let result = false; // true for player win and false for CPU win
 
-function getCpuChoice(max) { // get random number from 0 to max
-    randomNumber = Math.floor(Math.random() * max)
-    return randomNumber
-}
-// 0=paper 1=rock 2=scissors
-
-function playerChosePaper() {
-    alert("You chose paper!");
-    userGuess = "paper";
-    console.log("userGuess is currently " + userGuess);
-}
-
-function playerChoseScissors() {
-    alert("You chose scissors!");
-    userGuess = "scissors";
-    console.log("userGuess is currently " + userGuess);
-}
+function getCpuChoice(max) {
+    // get random number from 0 to max
+    let randomNumber = Math.floor(Math.random() * max);
+    return randomNumber;
+} // 0=paper 1=rock 2=scissors
 
 function playRound(userGuess, cpuChoice) {
-    console.log("User chose " + userGuess + " and CPU chose " + cpuChoice)
-    if (userGuess == "rock" & cpuChoice == 2) {
-        rounds = rounds+1
-        userScore = userScore+1
-        alert("User wins!")
-    } else if (userGuess == "rock" & cpuChoice == 0) {
-        rounds = rounds+1
-        cpuScore = cpuScore+1
-        alert("CPU Wins!")
-    } else if (userGuess == "rock" & cpuChoice == 1) {
-        rounds = rounds+1
-        cpuScore = cpuScore+1
-        alert("CPU Wins!")
-    } else if (userGuess == "paper" & cpuChoice == 1) {
-        rounds = rounds+1
-        userScore = userScore+1
-        alert("User wins!")
-    } else if (userGuess == "paper" & cpuChoice == 0) {
-        rounds = rounds+1
-        cpuScore = cpuScore+1
-        alert("CPU Wins!")
-    } else if (userGuess == "paper" & cpuChoice == 2) {
-        rounds = rounds+1
-        cpuScore = cpuScore+1
-        alert("CPU Wins!")
-    } else if (userGuess == "scissors" & cpuChoice == 0) {
-        rounds = rounds+1
-        userScore = userScore+1
-        alert("User wins!")
-    } else if (userGuess == "scissors" & cpuChoice == 1) {
-        rounds = rounds+1
-        cpuScore = cpuScore+1
-        alert("CPU Wins!")
-    } else {
-        rounds = rounds+1
-        cpuScore = cpuScore+1
-        alert("CPU Wins!")
+    console.log("User chose " + userGuess + " and CPU chose " + cpuChoice);
+
+    if (userGuess == "rock" && cpuChoice == 2) {
+        result = true;
+        cpuChoiceWord = "scissors";
+    } else if (userGuess == "rock" && cpuChoice == 0) {
+        result = false;
+        cpuChoiceWord = "paper";
+    } else if (userGuess == "rock" && cpuChoice == 1) {
+        result = false;
+        cpuChoiceWord = "rock";
+    } else if (userGuess == "paper" && cpuChoice == 1) {
+        result = true;
+        cpuChoiceWord = "rock";
+    } else if (userGuess == "paper" && cpuChoice == 0) {
+        result = false;
+        cpuChoiceWord = "paper";
+    } else if (userGuess == "paper" && cpuChoice == 2) {
+        result = false;
+        cpuChoiceWord = "scissors";
+    } else if (userGuess == "scissors" && cpuChoice == 0){
+        result = true;
+        cpuChoiceWord = "paper";
+    } else if (userGuess == "scissors" && cpuChoice == 1){
+        result = false;
+        cpuChoiceWord = "rock";
+    } else if (userGuess == "scissors" && cpuChoice == 2){
+        result = false;
+        cpuChoiceWord = "scissors";
     }
-    
+
+    if (result == true) {
+        resultWord = "won!"
+    } else if (result == false) {
+        resultWord = "lost :("
+    }
+
 }
-
-
+const container = document.querySelector(".container");
 const rockButton = document.querySelector("#rockBtn");
+
 rockButton.addEventListener("click", function() {
-    let cpuChoice = getCpuChoice(3)
+    let cpuChoice = getCpuChoice(3);
     userGuess = "rock";
-    playRound(userGuess,cpuChoice)
+    playRound(userGuess, cpuChoice);
+
+    let resultElement = document.createElement("h1");
+    resultElement.textContent = "Computer chose " + cpuChoiceWord + ", you " + resultWord;
+    container.appendChild(resultElement);
 });
 
 const paperButton = document.querySelector("#paperBtn");
+
 paperButton.addEventListener("click", function() {
-    let cpuChoice = getCpuChoice(3)
+    let cpuChoice = getCpuChoice(3);
     userGuess = "paper";
-    playRound(userGuess,cpuChoice)
-})
+    playRound(userGuess, cpuChoice);
+
+    let resultElement = document.createElement("h1");
+    resultElement.textContent = "Computer chose " + cpuChoiceWord + ", you " + resultWord;
+    container.appendChild(resultElement);
+});
 
 const scissorsButton = document.querySelector("#scissorsBtn");
+
 scissorsButton.addEventListener("click", function() {
-    let cpuChoice = getCpuChoice(3)
+    let cpuChoice = getCpuChoice(3);
     userGuess = "scissors";
-    playRound(userGuess,cpuChoice)
-})
+    playRound(userGuess, cpuChoice);
 
-
-
-// userGuess = prompt("Please enter your guess for this rounds : rock / paper / scissors", "")
-// console.log("user won " + userScore + " rounds")
-// console.log("cpu won " + cpuScore + " rounds")
+    let resultElement = document.createElement("h1");
+    resultElement.textContent = "Computer chose " + cpuChoiceWord + ", you " + resultWord;
+    container.appendChild(resultElement);
+});
